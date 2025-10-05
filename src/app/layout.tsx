244c69cc9667b9163c361f360dbcac62ad808493
Hiroshana <hiroshanawanni@gmail.com>
2025-10-06 00:41:55 +1100

    User sync added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import {
 } from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import UserSync from "@/components/UserSync";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -43,6 +44,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <UserSync />
           {children}
         </body>
       </html>
